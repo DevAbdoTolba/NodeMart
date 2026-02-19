29c263124bd0460185f9db27dfef46dcdfc32735
Abdo Tolba <DevAbdoTolba@gmail.com>
2026-02-19 14:51:54 +0200

    Add security definitions for token authentication in Swagger options

--- src/swagger.js
+++ src/swagger.js
@@ -5,6 +5,20 @@ const options = {
   definition: {
     openapi: '3.0.0',
   },
+      components: {
+      securitySchemes: {
+        tokenAuth: {
+          type: 'apiKey',
+          in: 'header',
+          name: 'token',
+        },
+      },
+    },
+    security: [
+      {
+        tokenAuth: [],
+      },
+    ],
   apis: ['./routes/*.js'],
 };
 
