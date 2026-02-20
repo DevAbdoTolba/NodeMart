caedcf42c508b1bd06a56c1a4139d2ac586c1f4a
Abdo Tolba <DevAbdoTolba@gmail.com>
2026-02-20 16:09:00 +0200

    Revert "implementation of feature/product-category-management (#23)"
    This reverts commit 372e594e0da7e931d29f0ee4c0a8142ddb560356.

--- src/controllers/productController.js
+++
@@ -1,8 +0,0 @@
-import Product from '../models/productModel.js';
-import * as factory from '../utils/handlerFactory.js'; // صححت المسار
-
-export const createProduct = factory.createOne(Product);
-export const getProduct = factory.getOne(Product, 'category'); // populate category
-export const getAllProducts = factory.getAll(Product);
-export const updateProduct = factory.updateOne(Product);
-export const deleteProduct = factory.deleteOne(Product);
