9d97c294e6db9c1fee8cb78ae0944dab7c8c4bbd
Abdo Tolba <DevAbdoTolba@gmail.com>
2026-02-22 17:27:45 +0200

    fix: email now validates email :)
    email was accepteing any  string :)

--- src/middlewares/validateEmail.js
+++ src/middlewares/validateEmail.js
@@ -1,9 +1,24 @@
 import userModel from "../models/userModel.js";
 import catchAsync from "../utils/catchAsync.js";
 import AppError from "../utils/appError.js";
+import joi from "joi";
+
+const emailSchema = joi.object({
+    email: joi.string().email().required().messages(
+        {
+            "string.email": "Invalid email format",
+            "string.required": "Email is required",
+        }
+    )
+}).unknown(true);
+
 
 export const validateEmail = catchAsync(async (req, res, next) => {
-    const normalizedEmail = typeof req.body.email === "string" ? req.body.email.toLowerCase().trim() : req.body.email;
+    const {error, value} = emailSchema.validate(req.body);
+    if(error) {
+        return next(new AppError(error.details[0].message, 400));
+    }
+    const normalizedEmail = value.email;
     let emailFound = await userModel.findOne({ email: normalizedEmail });
     if(emailFound) {
         return next(new AppError("email already exists", 400));
