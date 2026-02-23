5cd2527e6d2d17ad724e33a429f833512119b4de
Youssefmo7 <mommaradona@gmail.com>
2026-02-23 05:07:49 +0200

    added porduct and category authorization

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as productController from "../controllers/productController.js";
 import { validateCreateProduct, validateUpdateProduct, validateId } from "../middlewares/validationMiddleware.js";
+import { protect, restrictTo } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
@@ -22,6 +23,27 @@ const router = express.Router();
  *         description: List of all products
  */
 router.get("/", productController.getAllProducts);
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   get:
+ *     summary: Get single product
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Product data
+ */
+router.get("/:id", validateId(), productController.getProduct);
+
+router.use(protect);
+router.use(restrictTo("admin"));
 
 /**
  * @swagger
@@ -52,24 +74,6 @@ router.get("/", productController.getAllProducts);
  *         description: Product created successfully
  */
 router.post("/", validateCreateProduct, productController.createProduct);
-
-/**
- * @swagger
- * /api/products/{id}:
- *   get:
- *     summary: Get single product
- *     tags: [Products]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Product data
- */
-router.get("/:id", validateId(), productController.getProduct);
 
 /**
  * @swagger
--- src/utils/email.js
+++ src/utils/email.js
@@ -3,13 +3,14 @@ import jwt from 'jsonwebtoken'
 import emailTemplate from './emailTemplate.js'
 import dotenv from 'dotenv'
 
+dotenv.config();
 
 const transporter = nodeMailer.createTransport({
     service: 'gmail',
     secure: false,
     auth: {
-        user: "you.mwork@gmail.com",
-        pass: "cscb zwte rqay pxdy"
+        user: process.env.EMAIL_SENDER,
+        pass: process.env.EMAIL_PASSWD
     }
 });
 
@@ -17,7 +18,7 @@ export default async function sendEmail(email, name) {
     const encryptedEmail = jwt.sign({email: email}, process.env.TOKEN_SECRET_KEY, {expiresIn: '1h'});
     const emailInfo = await transporter.sendMail({
         to: email,
-        from: "you.mwork@gmail.com",
+        from: process.env.EMAIL_SENDER,
         subject: "SignedUp in NodeMart",
         text: "Thank You For Signing in NodeMart, Please Verify Your Email!",
         html: emailTemplate(encryptedEmail, name)
