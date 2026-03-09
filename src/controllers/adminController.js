f41e250ad97977050d782ec8709803614c2c6283
NESMA47 <nesmaeid422@gmail.com>
2026-03-09 20:40:00 +0200

    feat: add getAllUsers endpoint for admin management

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,6 +1,23 @@
 import Order from "../models/orderModel.js";
 import catchAsync from "../utils/catchAsync.js";
+import User from '../models/userModel.js';
 
+
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({
+            status: 'success',
+            results: users.length,
+            data: users 
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        });
+    }
+};
 export const getStats = catchAsync(async (req, res, next) => {
   const stats = await Order.aggregate([
     { $group: { _id: null, totalRevenue: { $sum: "$totalPrice" }, totalOrders: { $sum: 1 } } }
